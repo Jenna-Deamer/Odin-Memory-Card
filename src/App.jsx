280e9a60e6d8fc655a9d4f0fe8bbfbc5ded930d2
Jenna Deamer <jennadeamer@gmail.com>
2025-10-19 11:30:28 -0400

    Move api fetch and data handling to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
-import APIService from './APIService'
+import { useEffect, useState } from "react";
 
 function App() {
+    const [data, setData] = useState(null);
+    const limit = 10;
+    const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+            .then(response => response.json())
+            .then(data => setData(data))
+            .finally(() => setLoading(false))
+    }, [setData])
+
+    if (loading) return <div>Loading...</div>
 
     return (
         <>
-
             <h1>Memory Card Game</h1>
-            <APIService />
         </>
     )
 }
